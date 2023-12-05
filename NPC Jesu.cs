1b975af07b3315deb27970e42f064b10e5ba3a43
Vegeta777-YT <152323085+Vegeta777-YT@users.noreply.github.com>
2023-12-05 19:57:25 +0100

    Add files via upload

---
+++ NPC Jesu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCJesu : MonoBehaviour
+{
+    //NPC Health
+     public int health = 5;
+
+    //NPC Level
+     public int level = 1;
+
+    //NPC Speed
+   public float speed = 1.2f;
+
+    
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health += level;
+        print(health);
+    }
+}
